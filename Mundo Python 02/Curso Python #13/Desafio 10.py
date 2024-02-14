bea096fd1001ae3ad825ca97117a2462450c91fd
Bruno Venâncio <contatobrunovnutri@gmail.com>
2024-02-14 01:30:00 -0300

    Feat: Estrutura de Repetição For Final

--- Mundo Python 02/Curso Python #13/Desafio 10.py
+++ Mundo Python 02/Curso Python #13/Desafio 10.py
@@ -1,2 +1,18 @@
-for c in range(0, 5):
-    peso = float(input('Qual o seu peso atual? '))
+maior = 0
+menor = 0
+
+for p in range(1, 6):
+    peso = float(input(f'Peso do {p}° indivíduo: '))
+    if p == 1:
+        maior = peso
+        menor = peso
+    else:
+        if peso > maior:
+            maior = peso
+        if peso < menor:
+            menor = peso
+print(f'O maior peso é {maior}kg.')
+print(f'O menor peso é {menor}kg.')
+
+#Em resumo, se a primeira pessoa for o maior ou menor (if p == 1:) atribuindo o "maior" ou o "menor" a "peso".
+#Caso contrário, se o "peso" for maior que o maior "p" ou menor que o menor "p", "maior" e "menor" serão atribuidos a "peso".
---
+++ Mundo Python 02/Curso Python #13/Desafio 11.py
@@ -0,0 +1,29 @@
+somaidade = 0
+média = 0
+velho = 0
+nomevelho = ''
+
+mulher20 = 0
+
+for p in range(1, 5):
+    print(f'----- {p}° PESSOA -----')
+    nome = str(input(f'Nome: '))
+    idade = int(input(f'Idade: '))
+    sexo = str(input(f'Sexo [M/F]: '))
+    somaidade = somaidade + idade
+    if p == 1 and sexo in 'Mm':
+        velho = idade
+        nomevelho = nome
+    if sexo in 'Mm' and idade > velho:
+        velho = idade
+        nomevelho = nome
+    if sexo in 'Ff' and idade < 20:
+        mulher20 = mulher20 + 1
+#Se "p1" e "sexo" seja masculino: velho = idade e nomevelho = nome. (Testando se o primeiro indivíduo é o mais velho).
+#Caso contrário, se em "sexo" seja masculino e idade maior que velho (o primeiro indivíduo), então velho = idade e nomevelho = nome.
+#Se em "sexo" seja feminino e idade menor que 20: Acrescenta 1 em "mulher20".
+
+média = somaidade / 4
+print(f'A média de idade do grupo é de {média} anos.')
+print(f'O homem mais velho tem {velho} e se chama {nomevelho}.')
+print(f'No total existem {mulher20} mulheres com menos de 20 anos.')
